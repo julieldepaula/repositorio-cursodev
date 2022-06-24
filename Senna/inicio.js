bc1e714c5cc4a9da8e621ec5736dd91cbe91b8ae
Juliel de Paula <103391236+julieldepaula@users.noreply.github.com>
2022-06-23 21:47:00 -0300

    rascunho tela inicio

--- Senna/inicio.js
+++ Senna/inicio.js
@@ -1,2 +1,11 @@
 import React from 'react';
 import {View, Text, ImageBackground} from 'react-native';
+import telainicio from '../../assets/Telainicio.png';
+import style from './style';
+export default function TelaPrincipal (){
+    return(
+     <ImageBackground source = {Telainicio} style={style.background}>
+     
+     </ImageBackground>
+     )
+};
