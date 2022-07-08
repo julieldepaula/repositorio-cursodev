57ea54914c4ccd646d75ea86e31f8edca26b178a
Juliel de Paula <103391236+julieldepaula@users.noreply.github.com>
2022-07-08 10:37:25 -0300

    Inclusão de textos Tela Sobre

--- Senna/components/sobre/index.js
+++ Senna/components/sobre/index.js
@@ -16,22 +16,51 @@ export default function TelaSobre () {
 
             <Image source={fotocapa} style={style.img}/>
 
-            <Text style={style.conteudo}> conteudo </Text>
-
+            <Text style={style.conteudo}>
+             No esporte mais global e veloz do mundo, um piloto é considerado o mais rápido de todos os tempos: Ayrton Senna. Seus expressivos números ajudam a explicar porque o piloto ganhou status de mito do esporte. Mas Senna é mais do que isso: o brasileiro foi o responsável por alguns dos momentos mais mágicos da principal categoria do automobilismo mundial. 
+            </Text>
 
             <ScrollView>
+                <View>
+                  <Image source={imagem1} style={style.imgbox}/>
+                   <View>
+                    <Text>Rei da chuva</Text>
+                    <Text>
+                     Aprimorou a pilotagem no asfalto molhando e mostrou ser um piloto de determinação, garra e persistência.
+                    </Text>
+                   </View>
+                </View>
 
-                <Image source={imagem1} style={style.imgbox}/>
-                <Text>conteudo</Text>
-
-                <Image source={imagem2} style={style.imgbox}/>
-                <Text>conteudo</Text>
+                <View>
+                  <Image source={imagem2} style={style.imgbox}/>
+                   <View> 
+                    <Text>Rei de Monaco</Text>
+                    <Text>
+                    Conquistou o posto por ser o maior recordista de vitórias, com seis conquistas.
+                    </Text>
+                   </View>
+                </View>
                 
-                <Image source={imagem3} style={style.imgbox}/>
-                <Text>conteudo</Text>
+                <View>
+                  <Image source={imagem3} style={style.imgbox}/>
+                   <View>
+                    <Text>silvastone</Text>
+                    <Text>
+                     Por seu currículo impressionante em Silverstone, Ayrton recebeu o apelido de 'Silvastone' pela imprensa inglesa.
+                    </Text>
+                   </View>
+                </View>
 
-                <Image source={imagem4} style={style.imgbox}/>
-                <Text>conteudo</Text>
+                <View>
+                  <Image source={imagem4} style={style.imgbox}/>
+                  <View>
+                   <Text>preparacao</Text>
+                   <Text>
+                    Para vencer corridas e campeonatos o piloto precisava ter uma preparação física de atleta.
+                    </Text>
+                   </View>
+                 </View>
+                 
             </ScrollView>
 
 
