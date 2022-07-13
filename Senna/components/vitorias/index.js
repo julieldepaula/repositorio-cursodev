af69bf80bbedd7083a10da341c87a680208b7414
Juliel de Paula <103391236+julieldepaula@users.noreply.github.com>
2022-07-13 14:13:42 -0300

    Tela Vitorias finalizada
    projeto finalizado
    inclusão de estilos e pequenos ajustes.

--- Senna/components/vitorias/index.js
+++ Senna/components/vitorias/index.js
@@ -13,53 +13,54 @@ export default function TelaVitorias () {
     return (
     <ScrollView>  
     
-      <ImageBackground source={ Imagem } style={{flex:1}} blurRadius={5} >
+      <ImageBackground source={ Imagem } style={style.Fundo} blurRadius={5}>
 
-       <View>
-           <Text>Senna em Números</Text>
-           <Text>Ele conquistou três campeonatos mundiais em 1988, 1990 e 1991, e ganhou 41 Grandes Prêmios e 65 pole positions.</Text>
+       <View style={style.container}>
+
+           <Text style={style.titulo}>Senna em Números</Text>
+           <Text style={style.descrição}>Ele conquistou três campeonatos mundiais em 1988, 1990 e 1991, e ganhou 41 Grandes Prêmios e 65 pole positions.</Text>
           
-           <View>
+           <View style={style.icones}>
             <FontAwesome5 name="trophy" color="#ceb105" size={25} />
-            <Text>161</Text>
-            <Text>GPS DISPUTADOS</Text>
+            <Text style={style.numeros}>161</Text>
+            <Text style={style.textoicone}>GPS DISPUTADOS</Text>
           </View>
 
-          <View>
+          <View style={style.icones}>
             <FontAwesome5 name="trophy" color="#ceb105" size={25} />
-            <Text>65</Text>
-            <Text>POLE POSITIONS</Text>
+            <Text style={style.numeros}>65</Text>
+            <Text style={style.textoicone}>POLE POSITIONS</Text>
           </View>
 
-          <View>
+          <View style={style.icones}>
             <FontAwesome5 name="trophy" color="#ceb105" size={25} />
-            <Text>41</Text>
-            <Text>VITÓRIAS</Text>
+            <Text style={style.numeros}>41</Text>
+            <Text style={style.textoicone}>VITÓRIAS</Text>
           </View>
 
-          <View>
+          <View style={style.icones}>
             <FontAwesome5 name="trophy" color="#ceb105" size={25} />
-            <Text>3X</Text>
-            <Text>CAMPEÃO MUNDIAL</Text>
+            <Text style={style.numeros}>3X</Text>
+            <Text style={style.textoicone}>CAMPEÃO MUNDIAL</Text>
           </View>
 
        </View>
 
-       <View>
-           <Image source={Vitoria1}/>
-           <Text>Campeonato Mundial – 1988</Text>
+       <View style={style.containerbox}>           
+           <Text style={style.titulobox}>Campeonato Mundial – 1988</Text>
+           <Image source={Vitoria1} style={style.imgbox}/>
            
        </View>
 
-       <View>
-           <Image source={Vitoria2}/>
-           <Text>Campeonato Mundial – 1990</Text>
+       <View style={style.containerbox}>           
+           <Text style={style.titulobox}>Campeonato Mundial – 1990</Text>
+           <Image source={Vitoria2} style={style.imgbox}/>
            
        </View>
 
-       <View>
-           <Image source={Vitoria3}/>
-           <Text>Campeonato Mundial – 1991</Text>
+       <View style={style.containerbox}>           
+           <Text style={style.titulobox}>Campeonato Mundial – 1991</Text>
+           <Image source={Vitoria3} style={style.imgbox}/>
            
        </View>
        
--- Senna/components/vitorias/style.js
+++ Senna/components/vitorias/style.js
@@ -5,24 +5,68 @@ const style = StyleSheet.create ({
   Fundo:{
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center'
-  },
-  
-  img:{
-    
+    alignItems: 'center',
+    padding: 20
   },
   
   container:{
+    padding: 20,
+    backgroundColor: 'rgba(0,0,0,0.6)',
+    marginBottom: 50,    
     
   },
   
   titulo:{
-    
+    alignSelf: 'center',
+    fontSize: 22,
+    fontWeight: 'bold',
+    color: '#fff',
+    marginBottom: 10,
   },
   
   descrição:{
+  color: '#fff',
+  marginBottom: 20,
+  },
+
+  icones:{
+    alignItems: 'center',
+    flexDirection: 'row',
+    marginBottom: 10,
+  }, 
+
+  numeros:{
+    color: '#eecb01',
+    fontWeight: 'bold',
+    fontSize: 20,
+    marginLeft: 10,
+    marginRight: 5,
+  },
+
+  textoicone:{
+    color: '#a6a6a6',
+    fontSize: 16,
+  },
+
+  containerbox:{
+    backgroundColor: 'rgba(0,0,0,0.6)',
+    marginBottom: 20,
+  },
+
+  titulobox:{
+    alignSelf: 'center',
+    color: '#fff',
+    fontSize: 16,
+    padding: 15
     
   },
+
+  imgbox:{
+    height: 200,
+    width: 300
+    
+  },
+  
 });
 
 export default style;
